33343b3a1040bfb5817e56ef621d99dfb4a34ecb
IDG9I <damiangamerxd123@gmail.com>
2023-11-12 10:54:36 -0300

    Version 1.0.31

--- app/src/main/java/com/example/digitaldepth/MainActivity.java
+++ app/src/main/java/com/example/digitaldepth/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button2 = (Button)findViewById(R.id.button2);
+        button2 = (Button)findViewById(R.id.btnIniciarS);
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
